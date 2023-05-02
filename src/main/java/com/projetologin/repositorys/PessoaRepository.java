502d90fa4c2ac1c0ebd3f593474128964d4fa7bc
Tiago Souza <tiagoeduardosouza@gmail.com>
2023-05-02 17:54:29 -0300

    Adicionando annotation @Repository  na interface PessoaRepository

--- src/main/java/com/projetologin/repositorys/PessoaRepository.java
+++ src/main/java/com/projetologin/repositorys/PessoaRepository.java
@@ -1,9 +1,11 @@
 package com.projetologin.repositorys;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.projetologin.entites.Pessoa;
 
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>  {
 
 }
