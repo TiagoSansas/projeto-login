11d1f75ef1ae5cf8b15f51adc83026b22111486c
Tiago Souza <tiagoeduardosouza@gmail.com>
2023-05-02 18:24:22 -0300

    Criação do service Pessoa e o recurso para busca todas pessoas com paginação

---
+++ src/main/java/com/projetologin/resources/PessoaResource.java
@@ -0,0 +1,34 @@
+package com.projetologin.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetologin.dto.PessoaDTO;
+import com.projetologin.service.PessoaService;
+
+
+@RestController
+@RequestMapping("/pessoa")
+public class PessoaResource {
+
+	@Autowired
+	private PessoaService service;
+	
+	@GetMapping
+	ResponseEntity<Page<PessoaDTO>>findAllPAge(@RequestParam(defaultValue="0")Integer page,
+		@RequestParam(defaultValue="5") Integer linesPerPage,
+		@RequestParam(defaultValue="ASC")String direction,
+		@RequestParam(defaultValue="name")String orderBy){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction),orderBy);
+		Page<PessoaDTO> list = service.findAllPage(pageRequest);
+		return ResponseEntity.ok().body(list);
+	}
+}
---
+++ src/main/java/com/projetologin/service/PessoaService.java
@@ -0,0 +1,26 @@
+package com.projetologin.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.projetologin.dto.PessoaDTO;
+import com.projetologin.entites.Pessoa;
+import com.projetologin.repositorys.PessoaRepository;
+
+
+
+@Service
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository repository;
+	
+	@Transactional(readOnly=true)
+	public Page<PessoaDTO> findAllPage(PageRequest pageRequest){
+		Page<Pessoa> list = repository.findAll(pageRequest);
+		return list.map(x->new PessoaDTO(x));
+	}
+}
