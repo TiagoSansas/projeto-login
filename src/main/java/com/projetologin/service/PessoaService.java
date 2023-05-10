a11aacde78c84f474c6cc445fb8abea236de8b68
Tiago Souza <tiagoeduardosouza@gmail.com>
2023-05-10 18:55:47 -0300

    Criação o serviço de update para pessoa

--- src/main/java/com/projetologin/service/PessoaService.java
+++ src/main/java/com/projetologin/service/PessoaService.java
@@ -13,38 +13,45 @@ import com.projetologin.entites.Pessoa;
 import com.projetologin.repositorys.PessoaRepository;
 import com.projetologin.service.exceptions.ResourceNotFoundException;
 
-
-
 @Service
 public class PessoaService {
 
 	@Autowired
 	private PessoaRepository repository;
-	
-	@Transactional(readOnly=true)
-	public Page<PessoaDTO> findAllPage(PageRequest pageRequest){
+
+	@Transactional(readOnly = true)
+	public Page<PessoaDTO> findAllPage(PageRequest pageRequest) {
 		Page<Pessoa> list = repository.findAll(pageRequest);
-		return list.map(x->new PessoaDTO(x));
+		return list.map(x -> new PessoaDTO(x));
 	}
-	
-	@Transactional(readOnly =true)
+
+	@Transactional(readOnly = true)
 	public PessoaDTO findById(Long id) {
 		Optional<Pessoa> obj = repository.findById(id);
-		Pessoa entity = obj.orElseThrow(()->new ResourceNotFoundException("Entity not found"));
+		Pessoa entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new PessoaDTO(entity);
 	}
-	
+
 	@Transactional
 	public PessoaDTO insert(PessoaDTO dto) {
-		
+
 		Pessoa entity = new Pessoa();
 		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new PessoaDTO(entity);
-		
+
 	}
-	
-	public void copyDtoToEntity(PessoaDTO dto,Pessoa entity) {
+
+	@Transactional
+	public PessoaDTO update(Long id, PessoaDTO dto) {
+		Pessoa entity = repository.getReferenceById(id);
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new PessoaDTO(entity);
+
+	}
+
+	public void copyDtoToEntity(PessoaDTO dto, Pessoa entity) {
 		entity.setName(dto.getName());
 	}
 }
