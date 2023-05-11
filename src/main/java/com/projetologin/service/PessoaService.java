a00b15263aa2d052c49091d9a70682f8cf2882cc
Tiago Souza <tiagoeduardosouza@gmail.com>
2023-05-11 15:37:39 -0300

    Tramento de erro no service de Update e delete e a criação da exception Database

--- src/main/java/com/projetologin/service/PessoaService.java
+++ src/main/java/com/projetologin/service/PessoaService.java
@@ -3,6 +3,8 @@ package com.projetologin.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -11,7 +13,10 @@ import org.springframework.transaction.annotation.Transactional;
 import com.projetologin.dto.PessoaDTO;
 import com.projetologin.entites.Pessoa;
 import com.projetologin.repositorys.PessoaRepository;
+import com.projetologin.service.exceptions.DatabaseException;
 import com.projetologin.service.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class PessoaService {
@@ -44,16 +49,26 @@ public class PessoaService {
 
 	@Transactional
 	public PessoaDTO update(Long id, PessoaDTO dto) {
-		Pessoa entity = repository.getReferenceById(id);
-		copyDtoToEntity(dto, entity);
-		entity = repository.save(entity);
-		return new PessoaDTO(entity);
+		try {
+			Pessoa entity = repository.getReferenceById(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new PessoaDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 
 	}
-	
+
 	@Transactional
-	 public void delete(Long id) {
-		repository.deleteById(id);
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 
 	public void copyDtoToEntity(PessoaDTO dto, Pessoa entity) {
